0f13265272347e5d8899e8bddc8e2fc000a4e345
Kaustubh Nair <kaustubh.nair108@gmail.com>
2011-04-11 12:45:00 +0100

    Create models

--- api/models.py
+++ api/models.py
@@ -1,10 +1,66 @@
 from django.db import models
+import datetime
 
-# Create your models here.
 
-class Note(models.Model):
+class User(models.Model):
     name = models.CharField(max_length=100)
+    subscribed_tags = models.ManyToManyField("Tag")
+    favourite_books = models.ManyToManyField("Book", related_name='+')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    is_private = models.BooleanField(default=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=400)
+    is_public = models.BooleanField(default=False)
+    votes = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Note(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.TextField(default='', max_length=100000)
+    coordinates = models.CharField(default='', max_length=10000)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, blank=True, null=True)
+    parent = models.ForeignKey("Note", on_delete=models.SET_NULL, blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=300)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
